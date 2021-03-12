288e19bd438c6a30310cf938a41206cbe8e41de1
Pavlik <alaraf33@gmail.com>
2021-03-12 18:05:55 +0200

    verstka and git

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+import Header from "./Header";
+import Navbar from "./Navbar";
+import Profile from "./Profile";
+
+
+
+const App = () => {
+    return (
+        <div className ='app-wrapper'>
+            <Header />
+            <Navbar />
+            <Profile />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Header = () => {
+    return (
+            <header className='header'>
+                <img
+                    src='http://3.bp.blogspot.com/-iKHwpJF-EHo/UE17RbszugI/AAAAAAAAAN4/THNcHg8V2Zc/s1600/twitter-bird-light-bgs.png'/>
+            </header>
+    );
+};
+
+export default Header;
